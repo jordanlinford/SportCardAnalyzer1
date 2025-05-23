6ad530d9621424954129882c20ac690427f62c26
Jordan Linford <jordanreedlinford@gmail.com>
2025-05-22 21:47:55 -0600

    Update API URL to use Netlify Functions and fix CORS issues

---
+++ env.js
@@ -0,0 +1,10 @@
+window.API_URL = "/api";
+window.firebaseConfig = {
+  apiKey: "AIzaSyAfb2YtBxD5YEWrNpG0J3GN_g0ZfPzsoOE",
+  authDomain: "sports-card-analyzer.firebaseapp.com",
+  projectId: "sports-card-analyzer",
+  storageBucket: "sports-card-analyzer.appspot.com",
+  messagingSenderId: "27312906394",
+  appId: "1:27312906394:web:11296b8bb530daad5a7f23",
+  measurementId: "G-YNZTKCHQT0"
+}; 
